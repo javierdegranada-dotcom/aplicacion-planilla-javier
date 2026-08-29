f38165378195ac783e0b6395ff15ffda03dcb768
javierdegranada-dotcom <javierdegranada@gmail.com>
2026-08-29 20:08:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Estrategia: Cache-first (funciona sin internet)
    ================================================ */
 
-const CACHE_NAME = "arco-gaia-v26";
+const CACHE_NAME = "arco-gaia-v27";
 
 // Archivos que se guardan en caché al instalar
 const ARCHIVOS_CACHE = [
